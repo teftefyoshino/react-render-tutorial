88908fa31b7d1385903a3cbbf220aa294385a353
teftef-TY <t.yoshino58@gmail.com>
2022-11-24 00:31:20 +0900

    Fix warning body tag

--- tutorial/spa/src/view/components/user/detail.tsx
+++ tutorial/spa/src/view/components/user/detail.tsx
@@ -22,40 +22,42 @@ export const UserDetail: FC = () => {
             </p>
             {user ? (
                 <table>
-                    <tr>
-                        <th>ID</th>
-                        <td>{user.id}</td>
-                    </tr>
-                    <tr>
-                        <th>名前</th>
-                        <td>
-                            {user.lastName} {user.firstName}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>年齢</th>
-                        <td>{user.age}</td>
-                    </tr>
-                    <tr>
-                        <th>性別</th>
-                        <td>{user.gender}</td>
-                    </tr>
-                    <tr>
-                        <th>郵便番号</th>
-                        <td>{user.address.zipCode}</td>
-                    </tr>
-                    <tr>
-                        <th>都道府県</th>
-                        <td>{user.address.prefecture}</td>
-                    </tr>
-                    {user.phoneNumbers.map((phoneNumber) => (
-                        <tr key={phoneNumber.id}>
-                            <th>電話番号</th>
+                    <tbody>
+                        <tr>
+                            <th>ID</th>
+                            <td>{user.id}</td>
+                        </tr>
+                        <tr>
+                            <th>名前</th>
                             <td>
-                                {phoneNumber.type} {phoneNumber.number}
+                                {user.lastName} {user.firstName}
                             </td>
                         </tr>
-                    ))}
+                        <tr>
+                            <th>年齢</th>
+                            <td>{user.age}</td>
+                        </tr>
+                        <tr>
+                            <th>性別</th>
+                            <td>{user.gender}</td>
+                        </tr>
+                        <tr>
+                            <th>郵便番号</th>
+                            <td>{user.address.zipCode}</td>
+                        </tr>
+                        <tr>
+                            <th>都道府県</th>
+                            <td>{user.address.prefecture}</td>
+                        </tr>
+                        {user.phoneNumbers.map((phoneNumber) => (
+                            <tr key={phoneNumber.id}>
+                                <th>電話番号</th>
+                                <td>
+                                    {phoneNumber.type} {phoneNumber.number}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
                 </table>
             ) : (
                 <p>unregistered user</p>
