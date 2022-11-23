53c4b3f48dc6a0538f60a3fe2bb41c7ec1fb45a3
teftef-TY <t.yoshino58@gmail.com>
2022-11-24 00:13:19 +0900

    Add modules/static-data

---
+++ modules/static-data/user/user-detail/index.ts
@@ -0,0 +1,26 @@
+import { userData } from './data';
+
+interface Address {
+    zipCode: string;
+    prefecture: string;
+}
+
+export interface PhoneNumber {
+    id: string;
+    type: string;
+    number: string;
+}
+
+export interface Users {
+    id: string;
+    firstName: string;
+    lastName: string;
+    gender: string;
+    age: number;
+    address: Address;
+    phoneNumbers: Array<PhoneNumber>;
+}
+
+export function getUserDetailData(id: string): Users | undefined {
+    return userData.find((user) => user.id === id);
+}
---
+++ modules/static-data/user/users-json.d.ts
@@ -0,0 +1,14 @@
+declare module './users.json' {
+    interface Users {
+        id: number;
+        displayName: string;
+    }
+
+    interface Data {
+        data: Array<Users>;
+    }
+
+    const data: Data;
+
+    export default data;
+}
